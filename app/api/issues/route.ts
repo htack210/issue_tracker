9d62548f47a7cab5ba3763696bc6f0ca22f97584
htack210 <htack210@live.com>
2024-06-27 12:18:44 -0400

    Experimenting with toast messages.

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -12,18 +12,19 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
 
-    if (!validation.success)
+    if (!validation.success) {
+        // toast.error("Validation error. Title must be 1 to 255 chars. \nDescription cannot be empty.")
         return (
             NextResponse.json(validation.error.errors, { status: 400 })
-            // toast.error("Title must be 1 to 255 chars and description cannot be empty")
         )
+    }
 
     const newIssue = await prisma.issue.create({
         data: { title: body.title, description: body.description }
     });
 
+    // toast.success("New issue added.")
     return (
         NextResponse.json(newIssue, { status: 201 })
-        // toast.success("New issue added.")
     )
 };

300cfb9ec9c1e66c062aba712946c07a27b60ea1
htack210 <htack210@live.com>
2024-07-01 10:07:32 -0400

    Add edit button

--- app/components/ButtonSettings.tsx
+++ app/components/ButtonSettings.tsx
@@ -1,5 +1,5 @@
 const TailWindSettings =
-  "bg-sky-500 rounded-lg hover:bg-sky-700 text-white px-4 py-2";
+  "flex space-x-2 items-center bg-sky-500 rounded-lg hover:bg-sky-700 text-white px-4 py-2";
 
 export const ButtonTheme = () => {
   return TailWindSettings;

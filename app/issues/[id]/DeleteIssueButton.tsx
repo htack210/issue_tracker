e1b66244164d776a458f219dc471f021f0324dfd
htack210 <htack210@live.com>
2024-07-01 20:25:22 -0400

    Show confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { ButtonThemeRed } from "../../components/ButtonSettings";
 import { TrashIcon } from "@radix-ui/react-icons";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <button className={ButtonThemeRed()}>
-      <TrashIcon />
-      Delete Issue
-    </button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <button className={ButtonThemeRed()}>
+          <TrashIcon />
+          Delete Issue
+        </button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone!
+        </AlertDialog.Description>
+        <Flex direction="row" mt="4" gapX="3">
+          <AlertDialog.Cancel>
+            <Button variant="solid" color="yellow">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
